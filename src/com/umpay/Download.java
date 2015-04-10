d62cdefd59410024fbc73b59b2be1c6b1c5fa38f
northwind2010 <ygf_8888@163.com>
2015-04-10 10:53:38 +0800

    t1

--- src/com/umpay/Download.java
+++ src/com/umpay/Download.java
@@ -5,6 +5,7 @@ public class Download {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git test");
+		System.out.println("git test");
 	}
 
 }

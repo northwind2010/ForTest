1083492a50f1b526d193ecf3d08f4987c0936397
northwind2010 <ygf_8888@163.com>
2015-04-13 10:31:28 +0800

    test

--- src/com/umpay/ThreadTest.java
+++ src/com/umpay/ThreadTest.java
@@ -7,6 +7,17 @@ public class ThreadTest {
 		Thread t = Thread.currentThread();
 		t.setName("t1");
 		System.out.println(t.getName());
+		
+		new Thread(new Runnable() {
+			
+			public void run() {
+				// TODO Auto-generated method stub
+				Thread t = Thread.currentThread();
+//				t.setName("t2");
+				System.out.println(t.getName());
+			}
+		}).start();
+		System.out.println(t.getName());
 
 	}
 

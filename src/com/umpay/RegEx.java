7735c58559e1e79687be4bf7f7a9ad1f88f4aa09
northwind2010 <ygf_8888@163.com>
2015-04-14 17:00:14 +0800

    test1

--- src/com/umpay/RegEx.java
+++ src/com/umpay/RegEx.java
@@ -1,12 +1,14 @@
 package com.umpay;
 
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class RegEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "21";
+		String str = "3";
 		System.out.println(str.matches("0|1|2"));
 		
 	}
-
 }

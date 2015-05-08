e6456b1ea9a8bb487c0cd491519207e79f00784f
northwind2010 <ygf_8888@163.com>
2015-05-08 10:28:36 +0800

    11

--- src/com/umpay/trim.java
+++ src/com/umpay/trim.java
@@ -16,25 +16,28 @@ public class trim {
 //		String str=null;
 //		String[] array = str.split("\\|");
 //		System.out.println(array.length);
-		String str1 = "123";
-		String str2 = "123";
-//		System.out.println(str.trim());
-		LinkedList<String> list = new LinkedList<String>();
-		LinkedHashSet<String> set = new LinkedHashSet<String>();
-		set.add(str1);
-		set.add(null);
-		System.out.println(set.contains(str1));;
-		System.out.println();
-		HashMap<String, String> map = new HashMap<String, String>();
-		map.put(null, "123");
-		System.out.println(map.get(null));
-		Hashtable<String, String> table = new Hashtable<String, String>();
-//		table.put(null, "123");
-//		System.out.println(table.get(null));
-//		System.out.println(URLEncoder.encode("����","GBK"));
-		trim.test();
-		Long l = 11111l;
-		System.out.println(Long.toString(l, 16).toUpperCase());
+//		String str1 = "123";
+//		String str2 = "123";
+////		System.out.println(str.trim());
+//		LinkedList<String> list = new LinkedList<String>();
+//		LinkedHashSet<String> set = new LinkedHashSet<String>();
+//		set.add(str1);
+//		set.add(null);
+//		System.out.println(set.contains(str1));;
+//		System.out.println();
+//		HashMap<String, String> map = new HashMap<String, String>();
+//		map.put(null, "123");
+//		System.out.println(map.get(null));
+//		Hashtable<String, String> table = new Hashtable<String, String>();
+////		table.put(null, "123");
+////		System.out.println(table.get(null));
+////		System.out.println(URLEncoder.encode("����","GBK"));
+//		trim.test();
+//		Long l = 11111l;
+//		System.out.println(Long.toString(l, 16).toUpperCase());
+		
+		
+		System.out.println(("|".split("\\|")).length);
 	}
 	
 	public static void test() throws Exception{
